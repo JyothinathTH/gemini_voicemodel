dcdebe679939ffca9c43b7893ab4851ba5278eaf
Jyothinath T H <85237151+JyothinathTH@users.noreply.github.com>
2024-04-07 21:27:15 +0530

    Add files via upload

---
+++ gemai.py
@@ -0,0 +1,37 @@
+import speech_recognition as sr
+import pyttsx3 as ptx
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyCTo6ikG2zYMgnqHBjNqIlfIweFMXniTtw")
+model = genai.GenerativeModel('gemini-pro')
+recognizer = sr.Recognizer()
+engine = ptx.init()
+
+
+
+while True:
+    with sr.Microphone() as mic:
+        recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+        print("Speak something...")
+        audio = recognizer.listen(mic)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        text = text.lower()
+        print(f"You said: {text}")
+        response=model.generate_content(text)
+
+        # Convert text to speech
+        rs=response.text
+        rs=rs.lower()
+        engine.say(rs)
+        print(response.text)
+        engine.runAndWait()
+    except sr.UnknownValueError:
+        engine.say("Can't Hear")
+        print("Could not understand audio")
+        engine.runAndWait()
+    except sr.RequestError as e:
+        
+        engine.say("Something error has occured")
+        print(f"Error: {e}")
+        engine.runAndWait()
